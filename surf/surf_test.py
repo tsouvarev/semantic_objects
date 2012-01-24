66139f1ae84899f862497a63d5c873eae14a8d9d
minime <tsouvarev@mail.ru>
2012-01-24 01:58:42 -0500

    playing with sparql endpoints

--- surf/surf_test.py
+++ surf/surf_test.py
@@ -4,6 +4,8 @@
 import surf
 import surf.namespace as ns
 from rdflib import *
+import SPARQLWrapper as wrap
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 #f = open ("moodle.rdf", "w")
 
@@ -21,26 +23,52 @@ from rdflib import *
 
 #print r["yearValue"]#["http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine_YearValue"]
 
-store = surf.Store(reader = "rdflib",writer = "rdflib",rdflib_store = "IOMemory")
-store.load_triples(source = "moodle.owl")#"http://www.w3.org/People/Berners-Lee/card.rdf")
-session = surf.Session(store)
+#store = surf.Store(reader = "rdflib",writer = "rdflib",rdflib_store = "IOMemory")
+#store = surf.Store(reader = 'sparql_protocol',
+#					writer = 'sparql_protocol',
+#                   endpoint = 'http://fourstore.avalon.ru:80/sparql'
+#                   #default_graph = 'wine'
+#                   )
 
+#store.load_triples()
+#store.load_triples(source = "moodle.owl")#"http://www.w3.org/People/Berners-Lee/card.rdf")
+#session = surf.Session(store)
+#q = session.default_store.execute_sparql("SELECT ?s ?p ?o WHERE { ?s ?p ?o }")
 
-print session.default_store.size()
+#print list(q)#session.default_store.size()
 
-ns.register (base = "http://www.semanticweb.org/ontologies/2011/10/Ontology1322456044761.owl")
+#ns.register (wine = "http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine#")
 
-s = "Форум"
+#c = session.get_class (ns.WINE + "Wine")#s.encode('utf-8'))
+#print c.all().first()
 
-c = session.get_class (ns.BASE + "Hot_Potatoes_Quiz")#s.encode('utf-8'))
-print c.all().first()
+#print session.default_store.size()
 
-print session.default_store.size()
+#store.close()
 
-store.close()
+##########sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+##########sparql.setQuery("""
+##########    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+##########    SELECT ?label
+##########    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
+##########""")
+##########sparql.setReturnFormat(JSON)
+##########results = sparql.query().convert()
 
+##########for result in results["results"]["bindings"]:
+##########    print result["label"]["value"].encode ("utf-8")
 
+#wget fourstore.avalon.ru/sparql --post-data 'query=SELECT+%2A+WHERE+%7B+%3Fs+%3Fp+%3Fo+%7D'
 
+sparql = SPARQLWrapper("http://fourstore.avalon.ru:80/sparql") # fourstore.avalon.ru:80
+sparql.setQuery("SELECT * WHERE { ?s ?p ?o }")
+sparql.setReturnFormat(JSON)
+results = sparql.query()
+
+print results
+
+for result in results:#["results"]["bindings"]:
+    print result#["s"]["value"].encode ("utf-8")
 
 
 
