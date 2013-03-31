a66c7fb01136cb349df4bffedc048c7d4ffa2a30
tsouvarev <tsouvarev@mail.ru>
2013-03-31 23:22:54 +0000

    added support for AllegroGraph triplestore

--- SemanticObjects/Connection.py
+++ SemanticObjects/Connection.py
@@ -1,29 +1,23 @@
-#! /bin/python
 # -*- coding: utf-8 -*-
 
-#import SPARQLWrapper as wrap
 from DBBackends import *
 
-class Connection ():
 
-    def __init__ (self, backend):
-    
-        if not isinstance (backend, Backend): raise Exception ("Given wrong backend for connection: %s" % backend)
+class Connection():
+    def __init__(self, backend):
+        if not isinstance(backend, Backend):
+            raise Exception("Given wrong backend for connection: %s" % backend)
 
         self.db = backend
 
-    def query (self, query):
+    def query(self, query):
+        return self.db.query(query)
 
-#        print "q"
-        return self.db.query (query)
+    def insert(self, query):
+        return self.db.insert(query)
 
-    def insert (self, query):
-    
-        return self.db.insert (query)
-        
-    def delete (self, query):
-    
-        return self.db.delete (query)
+    def delete(self, query):
+        return self.db.delete(query)
 
 
 
--- SemanticObjects/DBBackends.py
+++ SemanticObjects/DBBackends.py
@@ -1,38 +1,46 @@
 #! /bin/python
 # -*- coding: utf-8 -*-
 
-from SPARQLWrapper import SPARQLWrapper, JSON, RDF, XML, TURTLE, N3
-from urllib2 import urlopen, HTTPError, URLError
+# from SPARQLWrapper import SPARQLWrapper, JSON, RDF, XML, TURTLE, N3
+from urllib2 import urlopen, URLError
 from urllib import urlencode
-from abc import abstractmethod as abstract
+
 
 class Backend(object):
-
-    @abstract
     def query(self, query):
-        return
+        raise NotImplementedError("Method '%s' was not implemented for '%s'" % ("query", self.__class__.__name__))
 
-    @abstract
     def ask(self, query):
-        return
-    
-    @abstract
+        raise NotImplementedError("Method '%s' was not implemented for '%s'" % ("ask", self.__class__.__name__))
+
     def insert(self, query):
-        return
+        raise NotImplementedError("Method '%s' was not implemented for '%s'" % ("insert", self.__class__.__name__))
 
-    @abstract
     def update(self, query):
-        return
+        raise NotImplementedError("Method '%s' was not implemented for '%s'" % ("update", self.__class__.__name__))
 
-    @abstract
     def delete(self, query):
-        return
+        raise NotImplementedError("Method '%s' was not implemented for '%s'" % ("delete", self.__class__.__name__))
+
+
+class AllegroBackend(Backend):
+
+    def __init__(self, address):
+        self.endpoint = SPARQLWrapper(address)
+
+    def query(self, query):
+        self.endpoint.setQuery(query)
+
+        self.endpoint.setReturnFormat(JSON)
+        results = self.endpoint.query().convert()
+
+        return results
+
 
 class VirtuosoBackend(Backend):
 
     def __init__(self, address):
-
-        self.endpoint = SPARQLWrapper(address+"/sparql/")
+        self.endpoint = SPARQLWrapper(address + "/sparql/")
 
     def query(self, query):
         super(VirtuosoBackend, self).query(query)
@@ -46,12 +54,11 @@ class VirtuosoBackend(Backend):
 
 
 class FourstoreSparqlBackend(Backend):
-
     def __init__(self, address):
-    
+
         self.address = address + "/update/"
-        self.endpoint = SPARQLWrapper(address+"/sparql/")
-        
+        self.endpoint = SPARQLWrapper(address + "/sparql/")
+
     # выполняет запрос, возвращает результат в виде почти прямого
     # переноса XML/RDF на списки и словари в Python
     def query(self, query):
@@ -73,35 +80,36 @@ class FourstoreSparqlBackend(Backend):
         results = self.endpoint.query().convert()
 
         return results["boolean"]
-        
-    def insert (self, query):
+
+    def insert(self, query):
 
         super(FourstoreSparqlBackend, self).insert(query)
 
         values = {"update": q}
         headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        
-        try: res = urlopen (self.address, urlencode (values))
-        except URLError, e: 
-            
-            raise Exception (e.reason)
+
+        try:
+            res = urlopen(self.address, urlencode(values))
+        except URLError, e:
+
+            raise Exception(e.reason)
         else:
             return res.read()
-    
-    
-    def delete (self, query):
 
-        super(FourstoreSparqlBackend, self).delete (query)
-    
+    def delete(self, query):
+
+        super(FourstoreSparqlBackend, self).delete(query)
+
         values = {"update": query}
         headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        
-        try: res = urlopen (self.address, urlencode (values))
-        except URLError, e: 
-            
+
+        try:
+            res = urlopen(self.address, urlencode(values))
+        except URLError, e:
+
             print e.reason
             return False
-        
+
         else:
             return res.read()
         
